10ffaff8e498a3d1b86cebba4da7457ece8a1ee5
tamal <tamal@appscode.com>
2017-10-25 16:09:52 -0700

    Add skeleton CCM for various cloud providers

---
+++ cloud/providers/hetzner/instances.go
@@ -0,0 +1,43 @@
+package hetzner
+
+import (
+	"github.com/appscode/pharm-controller-manager/cloud"
+	"k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/types"
+)
+
+func (c *Cloud) NodeAddresses(name types.NodeName) ([]v1.NodeAddress, error) {
+	return nil, cloud.ErrNotImplemented
+}
+
+func (c *Cloud) NodeAddressesByProviderID(providerID string) ([]v1.NodeAddress, error) {
+	return nil, cloud.ErrNotImplemented
+}
+
+func (c *Cloud) ExternalID(nodeName types.NodeName) (string, error) {
+	return "", cloud.ErrNotImplemented
+}
+
+func (c *Cloud) InstanceID(nodeName types.NodeName) (string, error) {
+	return "", cloud.ErrNotImplemented
+}
+
+func (c *Cloud) InstanceType(nodeName types.NodeName) (string, error) {
+	return "", cloud.ErrNotImplemented
+}
+
+func (c *Cloud) InstanceTypeByProviderID(providerID string) (string, error) {
+	return "", cloud.ErrNotImplemented
+}
+
+func (c *Cloud) AddSSHKeyToAllInstances(user string, keyData []byte) error {
+	return cloud.ErrNotImplemented
+}
+
+func (c *Cloud) CurrentNodeName(hostname string) (types.NodeName, error) {
+	return "", cloud.ErrNotImplemented
+}
+
+func (c *Cloud) InstanceExistsByProviderID(providerID string) (bool, error) {
+	return false, cloud.ErrNotImplemented
+}
---
+++ cloud/providers/softlayer/zones.go
@@ -0,0 +1,19 @@
+package softlayer
+
+import (
+	"github.com/appscode/pharm-controller-manager/cloud"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/kubernetes/pkg/cloudprovider"
+)
+
+func (c *Cloud) GetZone() (cloudprovider.Zone, error) {
+	return cloudprovider.Zone{}, cloud.ErrNotImplemented
+}
+
+func (c *Cloud) GetZoneByProviderID(providerID string) (cloudprovider.Zone, error) {
+	return cloudprovider.Zone{}, cloud.ErrNotImplemented
+}
+
+func (c *Cloud) GetZoneByNodeName(nodeName types.NodeName) (cloudprovider.Zone, error) {
+	return cloudprovider.Zone{}, cloud.ErrNotImplemented
+}

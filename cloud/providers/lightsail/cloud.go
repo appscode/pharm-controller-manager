a43469732d38e320eb963cd839627376866f7882
tamal <tamal@appscode.com>
2017-11-25 13:02:38 -0800

    Add CCm for AWS lightsail

---
+++ cloud/providers/lightsail/cloud.go
@@ -0,0 +1,88 @@
+package lightsail
+
+import (
+	"io"
+	"io/ioutil"
+
+	gv "github.com/JamesClonk/vultr/lib"
+	"github.com/ghodss/yaml"
+	"k8s.io/kubernetes/pkg/cloudprovider"
+	"k8s.io/kubernetes/pkg/controller"
+)
+
+const (
+	ProviderName = "lightsail"
+)
+
+type tokenSource struct {
+	Token string `json:"token" yaml:"token"`
+}
+
+type Cloud struct {
+	client        *gv.Client
+	instances     cloudprovider.Instances
+	zones         cloudprovider.Zones
+}
+
+func init() {
+	cloudprovider.RegisterCloudProvider(
+		ProviderName,
+		func(config io.Reader) (cloudprovider.Interface, error) {
+			return newCloud(config)
+		})
+}
+
+func newCloud(config io.Reader) (cloudprovider.Interface, error) {
+	tokenSource := &tokenSource{}
+	contents, err := ioutil.ReadAll(config)
+	if err != nil {
+		return nil, err
+	}
+
+	err = yaml.Unmarshal(contents, tokenSource)
+	if err != nil {
+		return nil, err
+	}
+
+	vultrClient := gv.NewClient(tokenSource.Token, &gv.Options{})
+	return &Cloud{
+		client:        vultrClient,
+		instances:     newInstances(vultrClient),
+		zones:         newZones(vultrClient),
+	}, nil
+}
+
+func (c *Cloud) Initialize(clientBuilder controller.ControllerClientBuilder) {
+}
+
+func (c *Cloud) LoadBalancer() (cloudprovider.LoadBalancer, bool) {
+	return nil, false
+}
+
+func (c *Cloud) Instances() (cloudprovider.Instances, bool) {
+	return c.instances, true
+}
+
+func (c *Cloud) Zones() (cloudprovider.Zones, bool) {
+	return c.zones, true
+}
+
+func (c *Cloud) Clusters() (cloudprovider.Clusters, bool) {
+	return nil, false
+}
+
+func (c *Cloud) Routes() (cloudprovider.Routes, bool) {
+	return nil, false
+}
+
+func (c *Cloud) ProviderName() string {
+	return ProviderName
+}
+
+func (c *Cloud) ScrubDNS(nameservers, searches []string) (nsOut, srchOut []string) {
+	return nil, nil
+}
+
+func (c *Cloud) HasClusterID() bool {
+	return false
+}
---
+++ cloud/providers/lightsail/zones.go
@@ -0,0 +1,69 @@
+package lightsail
+
+import (
+	"io/ioutil"
+	"net/http"
+	"strconv"
+
+	gv "github.com/JamesClonk/vultr/lib"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/kubernetes/pkg/cloudprovider"
+)
+
+const (
+	serverIDURL   = "http://169.254.169.254/v1/instanceid"
+	serverListURL = "https://api.vultr.com/v1/server/list"
+)
+
+type zones struct {
+	client *gv.Client
+}
+
+func newZones(client *gv.Client) cloudprovider.Zones {
+	return zones{client}
+}
+
+func (z zones) GetZone() (cloudprovider.Zone, error) {
+	subid, err := fetchServerID()
+	if err != nil {
+		return cloudprovider.Zone{}, err
+	}
+	server, err := serverByID(z.client, subid)
+	if err != nil {
+		return cloudprovider.Zone{}, err
+	}
+
+	return cloudprovider.Zone{Region: strconv.Itoa(server.RegionID)}, nil
+}
+
+func (z zones) GetZoneByProviderID(providerID string) (cloudprovider.Zone, error) {
+	id, err := serverIDFromProviderID(providerID)
+	if err != nil {
+		return cloudprovider.Zone{}, err
+	}
+	server, err := serverByID(z.client, id)
+	if err != nil {
+		return cloudprovider.Zone{}, err
+	}
+
+	return cloudprovider.Zone{Region: strconv.Itoa(server.RegionID)}, nil
+}
+
+func (z zones) GetZoneByNodeName(nodeName types.NodeName) (cloudprovider.Zone, error) {
+	server, err := serverByName(z.client, nodeName)
+	if err != nil {
+		return cloudprovider.Zone{}, err
+	}
+	return cloudprovider.Zone{Region: strconv.Itoa(server.RegionID)}, nil
+}
+
+func fetchServerID() (string, error) {
+	resp, err := http.Get(serverIDURL)
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	return string(body), err
+}

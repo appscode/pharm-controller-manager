adbcc084b81b9996c9419738a4c7a5c99f63271f
Tamal Saha <tamal@appscode.com>
2018-07-08 21:49:35 -0700

    Format shell scripts
    find hack | grep sh | xargs shfmt -l -w -ci -i 2 {}

--- hack/docker/setup.sh
+++ hack/docker/setup.sh
@@ -18,29 +18,29 @@ IMG=cloud-controller-manager
 DIST=$GOPATH/src/github.com/pharmer/cloud-controller-manager/dist
 mkdir -p $DIST
 if [ -f "$DIST/.tag" ]; then
-	export $(cat $DIST/.tag | xargs)
+  export $(cat $DIST/.tag | xargs)
 fi
 
 clean() {
-    pushd $GOPATH/src/github.com/pharmer/cloud-controller-manager/hack/docker
-    rm cloud-controller-manager Dockerfile
-    popd
+  pushd $GOPATH/src/github.com/pharmer/cloud-controller-manager/hack/docker
+  rm cloud-controller-manager Dockerfile
+  popd
 }
 
 build_binary() {
-    pushd $GOPATH/src/github.com/pharmer/cloud-controller-manager
-    ./hack/builddeps.sh
-    ./hack/make.py build
-    detect_tag $DIST/.tag
-    popd
+  pushd $GOPATH/src/github.com/pharmer/cloud-controller-manager
+  ./hack/builddeps.sh
+  ./hack/make.py build
+  detect_tag $DIST/.tag
+  popd
 }
 
 build_docker() {
-    pushd $GOPATH/src/github.com/pharmer/cloud-controller-manager/hack/docker
-    cp $DIST/cloud-controller-manager/cloud-controller-manager-alpine-amd64 cloud-controller-manager
-    chmod 755 cloud-controller-manager
+  pushd $GOPATH/src/github.com/pharmer/cloud-controller-manager/hack/docker
+  cp $DIST/cloud-controller-manager/cloud-controller-manager-alpine-amd64 cloud-controller-manager
+  chmod 755 cloud-controller-manager
 
-    cat >Dockerfile <<EOL
+  cat >Dockerfile <<EOL
 FROM alpine
 
 RUN set -x \
@@ -50,40 +50,40 @@ COPY cloud-controller-manager /usr/bin/cloud-controller-manager
 
 ENTRYPOINT ["cloud-controller-manager"]
 EOL
-    local cmd="docker build -t pharmer/$IMG:$TAG ."
-    echo $cmd; $cmd
+  local cmd="docker build -t pharmer/$IMG:$TAG ."
+  echo $cmd; $cmd
 
-    rm cloud-controller-manager Dockerfile
-    popd
+  rm cloud-controller-manager Dockerfile
+  popd
 }
 
 build() {
-    build_binary
-    build_docker
+  build_binary
+  build_docker
 }
 
 docker_push() {
-    if [ "$APPSCODE_ENV" = "prod" ]; then
-        echo "Nothing to do in prod env. Are you trying to 'release' binaries to prod?"
-        exit 0
-    fi
-    if [ "$TAG_STRATEGY" = "git_tag" ]; then
-        echo "Are you trying to 'release' binaries to prod?"
-        exit 1
-    fi
-    hub_canary
+  if [ "$APPSCODE_ENV" = "prod" ]; then
+    echo "Nothing to do in prod env. Are you trying to 'release' binaries to prod?"
+    exit 0
+  fi
+  if [ "$TAG_STRATEGY" = "git_tag" ]; then
+    echo "Are you trying to 'release' binaries to prod?"
+    exit 1
+  fi
+  hub_canary
 }
 
 docker_release() {
-    if [ "$APPSCODE_ENV" != "prod" ]; then
-        echo "'release' only works in PROD env."
-        exit 1
-    fi
-    if [ "$TAG_STRATEGY" != "git_tag" ]; then
-        echo "'apply_tag' to release binaries and/or docker images."
-        exit 1
-    fi
-    hub_up
+  if [ "$APPSCODE_ENV" != "prod" ]; then
+    echo "'release' only works in PROD env."
+    exit 1
+  fi
+  if [ "$TAG_STRATEGY" != "git_tag" ]; then
+    echo "'apply_tag' to release binaries and/or docker images."
+    exit 1
+  fi
+  hub_up
 }
 
 source_repo $@
